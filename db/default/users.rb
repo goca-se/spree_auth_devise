58990d42d6009ed6bc2d1d8e2f26775e1cd28f31
Isabella Santos <isabella.santos@gocase.com.br>
2025-02-21 15:55:49 -0300

    refactor: remove admin creation on seeds

--- db/default/users.rb
+++ db/default/users.rb
@@ -71,13 +71,13 @@ def create_admin_user
   end
 end
 
-if Spree::User.admin.empty?
-  create_admin_user
-else
-  puts 'Admin user has already been previously created.'
-  if agree('Would you like to create a new admin user? (yes/no)')
-    create_admin_user
-  else
-    puts 'No admin user created.'
-  end
-end
+# if Spree::User.admin.empty?
+#   create_admin_user
+# else
+#   puts 'Admin user has already been previously created.'
+#   if agree('Would you like to create a new admin user? (yes/no)')
+#     create_admin_user
+#   else
+#     puts 'No admin user created.'
+#   end
+# end
